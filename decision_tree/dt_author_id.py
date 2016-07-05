13c007758bd90f46a790aa3d9215e36936bb1524
Abhishek Singhania <abhisinghania14@gmail.com>
2016-07-05 09:08:18 +0530

    Decision Tree Testing

---
+++ decision_tree/dt_author_id.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+
+t0 = time()
+pred=clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
+
+print "No of features of data" , len(features_train[0])
+
+from sklearn.metrics import accuracy_score 
+print "Accuracy Score of Decision Tree Algo is ", accuracy_score(pred, labels_test)
+
+
+#########################################################
+### your code goes here ###
+
+
+#########################################################
+
+
